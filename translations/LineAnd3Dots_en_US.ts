05ea575431d4db32189407542b03d3d69b0d0fd7
avkultra <avk_job@mail.ru>
2026-04-30 18:30:02 +0300

    тестовые файлы

---
+++ translations/LineAnd3Dots_en_US.ts
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>CMainWindow</name>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="38"/>
+        <source>tr_Ready</source>
+        <translation>Ready. Use the mouse wheel to zoom</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="336"/>
+        <source>tr_OpenFile</source>
+        <translation>Select file with point coordinates</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="338"/>
+        <source>tr_FileExt</source>
+        <translation>&quot;All  files (*.json);;JSON files (*.json)</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="347"/>
+        <source>tr_SuccessFileOnen</source>
+        <translation>File loaded successfully:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="348"/>
+        <source>tr_DataLoaded</source>
+        <translation>Data loaded</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="349"/>
+        <source>tr_LoadPonts</source>
+        <translation>Loaded %1 points 
+Starting point: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="350"/>
+        <source>tr_Success</source>
+        <translation>Success</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="357"/>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="358"/>
+        <source>tr_Error</source>
+        <translation>Loading error</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="359"/>
+        <source>tr_UnSuccessLoadfile</source>
+        <translation>Failed to load file.</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="367"/>
+        <source>tr_Warning</source>
+        <translation>Warning</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="368"/>
+        <source>tr_BeforeLoadPoints</source>
+        <translation>First, load the file with points</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="372"/>
+        <source>tr_FindOfSolver</source>
+        <translation>Searching for solution..</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="383"/>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="385"/>
+        <source>tr_SuccessSolver</source>
+        <translation>Solution found</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="386"/>
+        <source>tr_SuccessSolverInfo</source>
+        <translation>The line passing through the starting point
+provides a cyclic traversal of all points.
+
+The red line shows the found solution.</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="390"/>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="391"/>
+        <source>tr_UnSuccessSolver</source>
+        <translation>Solution not found</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="392"/>
+        <source>tr_UnSuccessSolverInfo</source>
+        <translation>For this set of points there is no line
+that cyclically passes through all points.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="46"/>
+        <source>Панель инструментов</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="72"/>
+        <source>Загрузка файла с точками</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="87"/>
+        <source>Найти прямую</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="14"/>
+        <source>Line solver</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="69"/>
+        <source>Загрузить файл</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="84"/>
+        <source> Решить задачу</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="96"/>
+        <source>Приблизить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="105"/>
+        <source>🔍 -</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="108"/>
+        <source>Отдалить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="117"/>
+        <source>Сброс</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="120"/>
+        <source>Сбросить масштаб и позицию</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="135"/>
+        <source>Показать/скрыть сетку</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/LineAnd3Dots_ru_RU.ts
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>CMainWindow</name>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="38"/>
+        <source>tr_Ready</source>
+        <translation>Готов. Используйте колесо мыши для масштаба</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="336"/>
+        <source>tr_OpenFile</source>
+        <translation>Выберите файл с координатами точек</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="338"/>
+        <source>tr_FileExt</source>
+        <translation>Все файлы (*.json);;JSON файлы (*.json)</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="347"/>
+        <source>tr_SuccessFileOnen</source>
+        <translation>Файл загружен успешно:</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="348"/>
+        <source>tr_DataLoaded</source>
+        <translation>Данные загружены</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="349"/>
+        <source>tr_LoadPonts</source>
+        <translation>Загружено %1 точек
+Начальная точка: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="350"/>
+        <source>tr_Success</source>
+        <translation>Успех</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="357"/>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="358"/>
+        <source>tr_Error</source>
+        <translation>Ошибка загрузки</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="359"/>
+        <source>tr_UnSuccessLoadfile</source>
+        <translation>Не удалось загрузить файл.</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="367"/>
+        <source>tr_Warning</source>
+        <translation>Предупреждение</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="368"/>
+        <source>tr_BeforeLoadPoints</source>
+        <translation>Сначала загрузите файл с точкам</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="372"/>
+        <source>tr_FindOfSolver</source>
+        <translation>Поиск решения...</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="383"/>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="385"/>
+        <source>tr_SuccessSolver</source>
+        <translation>Решение найдено</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="386"/>
+        <source>tr_SuccessSolverInfo</source>
+        <translation>Прямая, проходящая через начальную точку,
+обеспечивает циклический обход всех точек.
+
+Красная линия показывает найденное решение.</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="390"/>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="391"/>
+        <source>tr_UnSuccessSolver</source>
+        <translation>Решение не найдено</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.cpp" line="392"/>
+        <source>tr_UnSuccessSolverInfo</source>
+        <translation>Для данного набора точек не существует прямой,
+которая бы циклически проходила через все точки.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="46"/>
+        <source>Панель инструментов</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="72"/>
+        <source>Загрузка файла с точками</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="87"/>
+        <source>Найти прямую</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="14"/>
+        <source>Line solver</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="69"/>
+        <source>Загрузить файл</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="84"/>
+        <source> Решить задачу</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="96"/>
+        <source>Приблизить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="105"/>
+        <source>🔍 -</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="108"/>
+        <source>Отдалить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="117"/>
+        <source>Сброс</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="120"/>
+        <source>Сбросить масштаб и позицию</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow/mainwindow.ui" line="135"/>
+        <source>Показать/скрыть сетку</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
